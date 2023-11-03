4f63c9e1eff965b00c28424dfb3a77cf940a799d
Elisa <elisath01@gmail.com>
2023-11-03 16:40:46 -0300

    Creando Aplicacion de Formulario con React y Bootstrap

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import RegistrationForm from './components/RegistrationForm';
+
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>Formulario de Registro</h1>
+      <RegistrationForm />
+    </div>
+  );
+}
+export default App;
